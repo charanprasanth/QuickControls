2566fe2ec718151be608476e99083f2ef88a8fc1
charanprasanth <charanprasanth2001@gmail.com>
2026-06-28 13:27:12 +0100

    NewChanges

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 
 android {
     namespace = "com.poc.quickcontrols"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.poc.quickcontrols"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
+        targetSdk = 35
+        versionCode = 2
         versionName = "1.0"
     }
 
